5ac8f27826fa358df71886dc4a26a4532ccd2d27
YounesLahrichi <younesl@uw.edu>
2023-10-24 14:55:27 -0700

    added dev dependencie

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -28,11 +28,11 @@ const Footer = () => {
                 </ul>
 
                 <div className="footer__social">
-                    <a href="https://github.com/YounesLahrichi" className="footer__social-link" target="_blank">
+                    <a href="https://github.com/YounesLahrichi" className="footer__social-link">
                         <i class="uil uil-github-alt"></i>
                     </a>
 
-                    <a href="https://www.linkedin.com/in/younes-lahrichi-0894721a1/" className="footer__social-link" target="_blank">
+                    <a href="https://www.linkedin.com/in/younes-lahrichi-0894721a1/" className="footer__social-link">
                         <i class="uil uil-linkedin"></i>
                     </a>
                 </div>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,11 +4,11 @@ const Social = () => {
     return(
         <div className="home__social">
             
-            <a href="https://github.com/YounesLahrichi" className="home__social-icon" target="_blank">
+            <a href="https://github.com/YounesLahrichi" className="home__social-icon">
                 <i class="uil uil-github-alt"></i>
             </a>
 
-            <a href="https://www.linkedin.com/in/younes-lahrichi-0894721a1/" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/younes-lahrichi-0894721a1/" className="home__social-icon">
                 <i class="uil uil-linkedin"></i>
             </a>
             
